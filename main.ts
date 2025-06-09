7974d47731bf2b13bbd383729f887183beed3f4f
Jackson McAfee <jacksonminecraft88@gmail.com>
2025-06-09 19:18:29 -0400

    Changed indents from spaces to tabs

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
 const indexHtml = await Deno.readTextFile("./index.html");
 
 Deno.serve(async (req) => {
-  const url = new URL(req.url);
-  if (url.pathname === "/projects") {
-    const api = "https://api.github.com/users/JackCraftCode/repos";
-    const resp = await fetch(api);
-    const repos = await resp.json();
-    return new Response(JSON.stringify(repos, null, 2), {
-      headers: { "content-type": "application/json" },
-    });
-  }
-  return new Response(indexHtml, {
-    headers: { "content-type": "text/html; charset=utf-8" },
-  });
+	const url = new URL(req.url);
+	if (url.pathname === "/projects") {
+		const api = "https://api.github.com/users/JackCraftCode/repos";
+		const resp = await fetch(api);
+		const repos = await resp.json();
+		return new Response(JSON.stringify(repos, null, 2), {
+			headers: { "content-type": "application/json" },
+		});
+	}
+	return new Response(indexHtml, {
+		headers: { "content-type": "text/html; charset=utf-8" },
+	});
 });
